f6b1f8669b5a0b96b27da5a9ed2b0a3f4437903e
jsk900 <jsk900@yahoo.co.uk>
2021-10-27 14:07:58 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from 'react'; //Import useState & useEffect from React
+
+//Import the provider
+import MyProvider from './context/MyProvider';
+
+//Import the components
+import Header from './components/Header';
+import Section from './components/Section';
+import Footer from './components/Footer';
+
+//Import the css
 import './App.css';
 
+//Define the App component
 const App = () => {
+  //Pass all relevant state and functions to the child components
   return (
-    <main>
-     <h1>Welcome to React</h1>
-    </main>
+    <MyProvider>
+      <main>
+        <Header />
+        <Section />
+        <Footer />
+      </main>
+    </MyProvider>
   );
-}
+};
 
 export default App;
-
